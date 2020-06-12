d2f0ec1c395981aa0e61340e2f3e925bb4531dca
shrutikabirje <64689497+shrutikabirje@users.noreply.github.com>
2020-06-12 19:51:51 +0530

    Update p5.py

--- p5.py
+++ p5.py
@@ -3,17 +3,16 @@ Given a two list. Create a third list by picking an odd-index element from the f
 l1= [3, 6, 9, 12, 15, 18, 21]
 l2= [4, 8, 12, 16, 20, 24, 28]
 l3= []
-
-odd=l1[1::2]
-print("Elements at odd-index from list one")
-print(odd)
-
-even= l2[0::2]
-print("Element at even-index from list two")
-print(even)
-
-print("Printing Final third list")
-l3.extend(odd)
-l3.extend(even)
-print(l3)
+l4=[]
+l5=[]
+for i in range (len(l1)):
+    if i%2!=0:
+        l3.append(l1[i])
+print("odd elements in list 1:",l3)
+for j in range (len(l2)):
+    if j%2==0:
+        l4.append(l2[j])
+print("even elements in list 2:",l4)
+l5=(l3+l4)
+print("Final list:",l5)
 
